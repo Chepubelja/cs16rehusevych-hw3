17b0807eaf0d68bd1b04c8bc2b427032245571a3
Chepubelja <rehusevych@ucu.edu.ua>
2016-11-19 14:51:02 +0200

    fixed whitespaces

--- src/main/java/ua/edu/ucu/smartarr/FilterDecorator.java
+++ src/main/java/ua/edu/ucu/smartarr/FilterDecorator.java
@@ -3,7 +3,7 @@ package ua.edu.ucu.smartarr;
 import ua.edu.ucu.functions.MyPredicate;
 
 // Tests every element and removes it if it doesn't satisfy MyPredicate
-public class FilterDecorator extends SmartArrayDecorator{
+public class FilterDecorator extends SmartArrayDecorator {
     protected MyPredicate myPredicate;
     public FilterDecorator(SmartArray smartArray, MyPredicate myPredicate) {
         super(smartArray);
@@ -13,8 +13,8 @@ public class FilterDecorator extends SmartArrayDecorator{
     public Object[] toArray() {
         Object[] filtered = new Object[size()];
         int counter = 0;
-        for (int i = 0; i < smartArray.size(); i++){
-            if (myPredicate.test(smartArray.toArray()[i])){
+        for (int i = 0; i < smartArray.size(); i++) {
+            if (myPredicate.test(smartArray.toArray()[i])) {
                 filtered[counter] = smartArray.toArray()[i];
                 counter++;
                 //filtered[i] = myFunction.apply(smartArray.toArray()[i]);
@@ -29,8 +29,8 @@ public class FilterDecorator extends SmartArrayDecorator{
 
     public int size() {
         int new_size = 0;
-        for (int i = 0; i < smartArray.size(); i++){
-            if (myPredicate.test(smartArray.toArray()[i])){
+        for (int i = 0; i < smartArray.size(); i++) {
+            if (myPredicate.test(smartArray.toArray()[i])) {
                 new_size++;
             }
         }
